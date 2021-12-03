6404e3b3a2184fdec4fd872d52d621fc2eaff963
Linnea <linnea.evanson8@gmail.com>
2021-12-03 19:08:50 +0100

    changed name for test pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
 from setuptools import setup
 
-setup(name='linear_regression',
+setup(name='linear_regression_evanson',  # change this to unique name for pip package
       install_requires=['numpy'])
